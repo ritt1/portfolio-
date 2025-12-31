fc154d09c9ed57910f364ad37fda17c712750f68
rit <164156809+ritt1@users.noreply.github.com>
2025-12-31 13:54:35 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener("click", e => {
-    e.preventDefault();
-    document.querySelector(link.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
-  });
-});
